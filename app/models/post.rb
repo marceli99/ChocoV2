5d852238e0d69ac2a62e81a7611cddcf44ec92a5
Marceli99 <marceli99@protonmail.com>
2020-02-15 23:03:02 +0100

    Add basic validation to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,4 @@
 class Post < ApplicationRecord
+  validates :title, :presence => true, :length => {:maximum => 50}, :allow_nil => false
+  validates :content, :presence => true, :allow_nil => false
 end
