ed8c49654d880db31b8eb9e24c5b15f9a696d67f
Marceli99 <marceli99@protonmail.com>
2020-02-15 22:12:58 +0100

    Add user-friendly login path

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path_names: {
+      sign_in: 'login'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'application#index'
   # Admin #
   get 'admin/' => 'admin#index', as: :admin
   resources :posts
+  as :user do
+    get 'login/' => 'devise/sessions#new', as: :login
+  end
 end
