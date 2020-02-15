473cd549e4a82208aeb71c317f1d76d8dd68c8ab
Marceli99 <marceli99@protonmail.com>
2020-02-15 23:03:12 +0100

    Add basic post model test

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "validation" do
+    before(:each) do
+      @post = Post.new(title: nil, content: 'Lorem ipsum').save
+      @post2 = Post.new(title: 'Title #2', content: nil).save
+      @post3 = Post.new(title: 'Title #3',content: 'Lorem ipsum')
+    end
+    it "should have title" do
+      expect(@post).to eq(false)
+    end
+    it "should have content" do
+      expect(@post2).to eq(false)
+    end
+    it "title should have less than 50 characters" do
+      expect(@post3.title.length).to be <= 50
+    end
+  end
 end
