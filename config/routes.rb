2eb02cb81d2d8f8c390dc5262f5c55ce89d9d374
Marceli99 <marceli99@protonmail.com>
2020-02-16 22:49:24 +0100

    Remove unused admin controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,7 @@ Rails.application.routes.draw do
   }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'application#index'
-  # Admin #
-  get 'admin/' => 'admin#index', as: :admin
+  # Posts
   resources :posts
   as :user do
     get 'login/' => 'devise/sessions#new', as: :login
