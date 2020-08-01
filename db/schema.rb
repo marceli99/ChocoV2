83b0e063c733e10acb97468ea083a2087748ff4e
Marcel <marceli99@protonmail.com>
2020-08-01 16:33:03 +0200

    update(schema.rb): missing table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_22_221958) do
+ActiveRecord::Schema.define(version: 2020_05_23_211225) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -36,6 +36,15 @@ ActiveRecord::Schema.define(version: 2020_05_22_221958) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "settings", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.string "keywords"
+    t.string "language"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
