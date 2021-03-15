3e7629664268e089b56fd382d535d60e624233a5
Marcel Hasiński <marcel.hasinski@programa.pl>
2021-03-15 12:57:50 +0100

    refactor(db/seeds.rb): better seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 user = User.new
 user.email = 'admin@admin.admin'
 user.password = 'zaq1@WSX'
@@ -13,7 +5,13 @@ user.password_confirmation = 'zaq1@WSX'
 user.save!
 
 post = Post.new
-post.content = 'Test #1'
-post.created_at = DateTime.new(2021, 1, 1, 0, 0)
+post.content = IO.binread("./db/files/post.txt")
+post.created_at = DateTime.new(2020, 12, 31, 21, 15)
 post.title = 'Test #1'
+post.save!
+
+post = Post.new
+post.content = IO.binread("./db/files/post.txt")
+post.created_at = DateTime.new(2020, 12, 31, 15, 0)
+post.title = 'Test #2'
 post.save!
